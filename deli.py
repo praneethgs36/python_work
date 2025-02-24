465b90bddffde8561f42dbab58837e97f2811bd8
Praneeth <praneethgs36@gmail.com>
2025-02-24 22:54:51 +0530

    new files added

--- deli.py
+++ deli.py
@@ -1,3 +1,25 @@
-sandwich_orders = []
+sandwich_orders = ['pastrami', 
+                   'tuna', 
+                   'pastrami', 
+                   'bacon', 
+                   'pastrami', 
+                   'cheese', 
+                   'beef',
+                   ]
+
 
 finished_sanwiches = []
+
+print("Pastrami is out of stock. ")
+
+while sandwich_orders:
+    if 'pastrami' in sandwich_orders:
+        sandwich_orders.remove('pastrami')
+    else:
+        finished_sandwich = sandwich_orders.pop()
+        print(f"Making {finished_sandwich} sandwich. ")
+        finished_sanwiches.append(finished_sandwich)
+
+print("\nThe following sandwich orders are ready to deliver:")
+for sandwich in finished_sanwiches:
+    print(sandwich)
