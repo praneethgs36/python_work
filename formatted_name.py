b12999f800035aac073b08528986007db42dbd88
Praneeth <Praneeth@KAPPSOFT.LOCAL>
2025-02-25 17:55:31 +0530

    changes made

--- formatted_name.py
+++ formatted_name.py
@@ -3,10 +3,13 @@ def format_name(first_name, last_name, middle_name=''):
     full_name = f"{first_name} {middle_name} {last_name}"
     return full_name.title()
 
-f_name = input("Enter your first name. ")
-m_name = input("Enter your middle name. Press enter if not applicable. ")
-l_name = input("Enter your last name. ")
-
-musician = format_name(f_name, l_name, m_name)
-print(musician)
-
+while True:
+    print("Tell us your name. Enter 'quit' to leave the program. ")
+    f_name = " "
+    if f_name != 'quit':
+        f_name = input("your first name. ")
+        m_name = input("your middle name. Press enter if not applicable. ")
+        l_name = input("your last name. ")
+        print(f"Hello, {format_name(f_name, l_name, m_name)}")
+    else:
+        break
