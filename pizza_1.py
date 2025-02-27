1eb19e665c02deafdf463100b9428218db3e8b2a
Praneeth <Praneeth@KAPPSOFT.LOCAL>
2025-02-27 16:53:07 +0530

    changes

--- pizza_1.py
+++ pizza_1.py
@@ -1,8 +1,8 @@
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Print the list of toppings requested. """
-    print("Making a pizza with the following toppings. ")
+    print(f"\nMaking a {size}-inch pizza with the following toppings. ")
     for topping in toppings:
         print(f"- {topping}")
 
-make_pizza('peppermint')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
