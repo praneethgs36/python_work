39d333cf1c7283523fa42540c4869b3cce050b19
praneethgs33 <your-email@example.com>
2025-03-09 08:53:12 +0530

    changes

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
 path = Path('text_files/pi_digits.txt')
-contents = path.read_text().rstrip()
-print(contents)
+contents = path.read_text()
+
+lines = contents.splitlines()
+print(lines)
+for line in lines:
+    print(line)
+
+# print(contents)
