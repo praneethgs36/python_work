35496b230fdbd3e4bc84118cc4d43fdf8570c93b
praneethgs33 <your-email@example.com>
2025-03-09 22:19:31 +0530

    new files

--- file_reader.py
+++ file_reader.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
-path = Path('text_files/pi_digits.txt')
+path = Path('text_files/pi_million_digits.txt')
 contents = path.read_text()
 
 lines = contents.splitlines()
-print(lines)
+
+pi_string = ""
 for line in lines:
-    print(line)
+    pi_string += line.lstrip()
+
+print(f"{pi_string[0:52]}... ... ...")
+print(f"The length of pi_string is {len(pi_string)}")
 
 # print(contents)
