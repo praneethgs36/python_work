ad999291a73ae80a098b132ace37edea3a9dc410
Praneeth <Praneeth@KAPPSOFT.LOCAL>
2025-03-13 17:51:19 +0530

    changes

--- guest_file.py
+++ guest_file.py
@@ -3,10 +3,15 @@ This program prompts the user for their name and
 writes it on a text file. "
 """
 from pathlib import Path
-
-prompt = "Please enter you full name: "
-name = input(prompt)
-
-path = Path("text_files/guest.txt")
-path.write_text(name)
-
+path = Path("text_files/guest_book.txt")
+book =""
+    
+while True:
+    prompt = "Please enter you full name. Enter 'q' if you want to quit: "
+    name = input(prompt)
+    if name == 'q':
+        break
+    else:
+        book += name + '\n'
+        path.write_text(book)
+        print("Your name is added to the guests list. ")
